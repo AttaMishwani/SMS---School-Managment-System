4bd9d5103d8354bd8289de35f10510e965530fef
Atta Ur Rehman <atta.rehmanmishwani@gmail.com>
2026-04-02 19:04:43 +0500

    student list in admin

--- backend/src/controllers/studentsListController.js
+++ backend/src/controllers/studentsListController.js
@@ -4,20 +4,16 @@ const Student = require("../models/student-model");
 const studentsList = async (req, res) => {
     try {
         const schoolId = req.user.schoolId;
-        console.log("schoolId from token:", schoolId); // is this correct?
+        console.log("schoolId from token:", schoolId); 
 
-        // fetch ALL students temporarily, ignore schoolId
-        const allStudents = await Student.find({});
+      
+        const allStudents = await Student.find({schoolId: schoolId});
         console.log("all students in DB:", allStudents.length);
 
-        const filtered = await Student.find({ schoolId: schoolId });
-        console.log("filtered students:", filtered.length);
-
-        res.status(200).json({
+      return  res.status(200).json({
             schoolId,
             allStudents: allStudents.length,
-            filtered: filtered.length,
-            students: filtered
+            students: allStudents
         });
     } catch (error) {
         console.log(error);
